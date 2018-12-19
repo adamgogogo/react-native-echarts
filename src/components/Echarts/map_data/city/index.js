f903183209d8e5df2268b63086beab7b4e42cac7
internet5 <172039711@qq.com>
2018-12-19 17:32:37 +0800

    Add files via upload

---
+++ src/components/Echarts/map_data/city/index.js
@@ -0,0 +1,691 @@
+import c110100 from './110100.json'
+import c120100 from './120100.json'
+import c130100 from './130100.json'
+import c130200 from './130200.json'
+import c130300 from './130300.json'
+import c130400 from './130400.json'
+import c130500 from './130500.json'
+import c130600 from './130600.json'
+import c130700 from './130700.json'
+import c130800 from './130800.json'
+import c130900 from './130900.json'
+import c131000 from './131000.json'
+import c131100 from './131100.json'
+import c140100 from './140100.json'
+import c140200 from './140200.json'
+import c140300 from './140300.json'
+import c140400 from './140400.json'
+import c140500 from './140500.json'
+import c140600 from './140600.json'
+import c140700 from './140700.json'
+import c140800 from './140800.json'
+import c140900 from './140900.json'
+import c141000 from './141000.json'
+import c141100 from './141100.json'
+import c150100 from './150100.json'
+import c150200 from './150200.json'
+import c150300 from './150300.json'
+import c150400 from './150400.json'
+import c150500 from './150500.json'
+import c150600 from './150600.json'
+import c150700 from './150700.json'
+import c150800 from './150800.json'
+import c150900 from './150900.json'
+import c152200 from './152200.json'
+import c152500 from './152500.json'
+import c152900 from './152900.json'
+import c210100 from './210100.json'
+import c210200 from './210200.json'
+import c210300 from './210300.json'
+import c210400 from './210400.json'
+import c210500 from './210500.json'
+import c210600 from './210600.json'
+import c210700 from './210700.json'
+import c210800 from './210800.json'
+import c210900 from './210900.json'
+import c211000 from './211000.json'
+import c211100 from './211100.json'
+import c211200 from './211200.json'
+import c211300 from './211300.json'
+import c211400 from './211400.json'
+import c220100 from './220100.json'
+import c220200 from './220200.json'
+import c220300 from './220300.json'
+import c220400 from './220400.json'
+import c220500 from './220500.json'
+import c220600 from './220600.json'
+import c220700 from './220700.json'
+import c220800 from './220800.json'
+import c222400 from './222400.json'
+import c230100 from './230100.json'
+import c230200 from './230200.json'
+import c230300 from './230300.json'
+import c230400 from './230400.json'
+import c230500 from './230500.json'
+import c230600 from './230600.json'
+import c230700 from './230700.json'
+import c230800 from './230800.json'
+import c230900 from './230900.json'
+import c231000 from './231000.json'
+import c231100 from './231100.json'
+import c231200 from './231200.json'
+import c232700 from './232700.json'
+import c310100 from './310100.json'
+import c310200 from './310200.json'
+import c320100 from './320100.json'
+import c320200 from './320200.json'
+import c320300 from './320300.json'
+import c320400 from './320400.json'
+import c320500 from './320500.json'
+import c320600 from './320600.json'
+import c320700 from './320700.json'
+import c320800 from './320800.json'
+import c320900 from './320900.json'
+import c321000 from './321000.json'
+import c321100 from './321100.json'
+import c321200 from './321200.json'
+import c321300 from './321300.json'
+import c330100 from './330100.json'
+import c330200 from './330200.json'
+import c330300 from './330300.json'
+import c330400 from './330400.json'
+import c330500 from './330500.json'
+import c330600 from './330600.json'
+import c330700 from './330700.json'
+import c330800 from './330800.json'
+import c330900 from './330900.json'
+import c331000 from './331000.json'
+import c331100 from './331100.json'
+import c340100 from './340100.json'
+import c340200 from './340200.json'
+import c340300 from './340300.json'
+import c340400 from './340400.json'
+import c340500 from './340500.json'
+import c340600 from './340600.json'
+import c340700 from './340700.json'
+import c340800 from './340800.json'
+import c341000 from './341000.json'
+import c341100 from './341100.json'
+import c341200 from './341200.json'
+import c341300 from './341300.json'
+import c341500 from './341500.json'
+import c341600 from './341600.json'
+import c341700 from './341700.json'
+import c341800 from './341800.json'
+import c350100 from './350100.json'
+import c350200 from './350200.json'
+import c350300 from './350300.json'
+import c350400 from './350400.json'
+import c350500 from './350500.json'
+import c350600 from './350600.json'
+import c350700 from './350700.json'
+import c350800 from './350800.json'
+import c350900 from './350900.json'
+import c360100 from './360100.json'
+import c360200 from './360200.json'
+import c360300 from './360300.json'
+import c360400 from './360400.json'
+import c360500 from './360500.json'
+import c360600 from './360600.json'
+import c360700 from './360700.json'
+import c360800 from './360800.json'
+import c360900 from './360900.json'
+import c361000 from './361000.json'
+import c361100 from './361100.json'
+import c370100 from './370100.json'
+import c370200 from './370200.json'
+import c370300 from './370300.json'
+import c370400 from './370400.json'
+import c370500 from './370500.json'
+import c370600 from './370600.json'
+import c370700 from './370700.json'
+import c370800 from './370800.json'
+import c370900 from './370900.json'
+import c371000 from './371000.json'
+import c371100 from './371100.json'
+import c371200 from './371200.json'
+import c371300 from './371300.json'
+import c371400 from './371400.json'
+import c371500 from './371500.json'
+import c371600 from './371600.json'
+import c371700 from './371700.json'
+import c410100 from './410100.json'
+import c410200 from './410200.json'
+import c410300 from './410300.json'
+import c410400 from './410400.json'
+import c410500 from './410500.json'
+import c410600 from './410600.json'
+import c410700 from './410700.json'
+import c410800 from './410800.json'
+import c410900 from './410900.json'
+import c411000 from './411000.json'
+import c411100 from './411100.json'
+import c411200 from './411200.json'
+import c411300 from './411300.json'
+import c411400 from './411400.json'
+import c411500 from './411500.json'
+import c411600 from './411600.json'
+import c411700 from './411700.json'
+import c420100 from './420100.json'
+import c420200 from './420200.json'
+import c420300 from './420300.json'
+import c420500 from './420500.json'
+import c420600 from './420600.json'
+import c420700 from './420700.json'
+import c420800 from './420800.json'
+import c420900 from './420900.json'
+import c421000 from './421000.json'
+import c421100 from './421100.json'
+import c421200 from './421200.json'
+import c421300 from './421300.json'
+import c422800 from './422800.json'
+import c429000 from './429000.json'
+import c430100 from './430100.json'
+import c430200 from './430200.json'
+import c430300 from './430300.json'
+import c430400 from './430400.json'
+import c430500 from './430500.json'
+import c430600 from './430600.json'
+import c430700 from './430700.json'
+import c430800 from './430800.json'
+import c430900 from './430900.json'
+import c431000 from './431000.json'
+import c431100 from './431100.json'
+import c431200 from './431200.json'
+import c431300 from './431300.json'
+import c433100 from './433100.json'
+import c440100 from './440100.json'
+import c440200 from './440200.json'
+import c440300 from './440300.json'
+import c440400 from './440400.json'
+import c440500 from './440500.json'
+import c440600 from './440600.json'
+import c440700 from './440700.json'
+import c440800 from './440800.json'
+import c440900 from './440900.json'
+import c441200 from './441200.json'
+import c441300 from './441300.json'
+import c441400 from './441400.json'
+import c441500 from './441500.json'
+import c441600 from './441600.json'
+import c441700 from './441700.json'
+import c441800 from './441800.json'
+import c441900 from './441900.json'
+import c442000 from './442000.json'
+import c445100 from './445100.json'
+import c445200 from './445200.json'
+import c445300 from './445300.json'
+import c450100 from './450100.json'
+import c450200 from './450200.json'
+import c450300 from './450300.json'
+import c450400 from './450400.json'
+import c450500 from './450500.json'
+import c450600 from './450600.json'
+import c450700 from './450700.json'
+import c450800 from './450800.json'
+import c450900 from './450900.json'
+import c451000 from './451000.json'
+import c451100 from './451100.json'
+import c451200 from './451200.json'
+import c451300 from './451300.json'
+import c451400 from './451400.json'
+import c460100 from './460100.json'
+import c460200 from './460200.json'
+import c460300 from './460300.json'
+import c469000 from './469000.json'
+import c500100 from './500100.json'
+import c510100 from './510100.json'
+import c510300 from './510300.json'
+import c510400 from './510400.json'
+import c510500 from './510500.json'
+import c510600 from './510600.json'
+import c510700 from './510700.json'
+import c510800 from './510800.json'
+import c510900 from './510900.json'
+import c511000 from './511000.json'
+import c511100 from './511100.json'
+import c511300 from './511300.json'
+import c511400 from './511400.json'
+import c511500 from './511500.json'
+import c511600 from './511600.json'
+import c511700 from './511700.json'
+import c511800 from './511800.json'
+import c511900 from './511900.json'
+import c512000 from './512000.json'
+import c513200 from './513200.json'
+import c513300 from './513300.json'
+import c513400 from './513400.json'
+import c520100 from './520100.json'
+import c520200 from './520200.json'
+import c520300 from './520300.json'
+import c520400 from './520400.json'
+import c522200 from './522200.json'
+import c522300 from './522300.json'
+import c522400 from './522400.json'
+import c522600 from './522600.json'
+import c522700 from './522700.json'
+import c530100 from './530100.json'
+import c530300 from './530300.json'
+import c530400 from './530400.json'
+import c530500 from './530500.json'
+import c530600 from './530600.json'
+import c530700 from './530700.json'
+import c530800 from './530800.json'
+import c530900 from './530900.json'
+import c532300 from './532300.json'
+import c532500 from './532500.json'
+import c532600 from './532600.json'
+import c532800 from './532800.json'
+import c532900 from './532900.json'
+import c533100 from './533100.json'
+import c533300 from './533300.json'
+import c533400 from './533400.json'
+import c540100 from './540100.json'
+import c542100 from './542100.json'
+import c542200 from './542200.json'
+import c542300 from './542300.json'
+import c542400 from './542400.json'
+import c542500 from './542500.json'
+import c542600 from './542600.json'
+import c610100 from './610100.json'
+import c610200 from './610200.json'
+import c610300 from './610300.json'
+import c610400 from './610400.json'
+import c610500 from './610500.json'
+import c610600 from './610600.json'
+import c610700 from './610700.json'
+import c610800 from './610800.json'
+import c610900 from './610900.json'
+import c611000 from './611000.json'
+import c620100 from './620100.json'
+import c620200 from './620200.json'
+import c620300 from './620300.json'
+import c620400 from './620400.json'
+import c620500 from './620500.json'
+import c620600 from './620600.json'
+import c620700 from './620700.json'
+import c620800 from './620800.json'
+import c620900 from './620900.json'
+import c621000 from './621000.json'
+import c621100 from './621100.json'
+import c621200 from './621200.json'
+import c622900 from './622900.json'
+import c623000 from './623000.json'
+import c630100 from './630100.json'
+import c632100 from './632100.json'
+import c632200 from './632200.json'
+import c632300 from './632300.json'
+import c632500 from './632500.json'
+import c632600 from './632600.json'
+import c632700 from './632700.json'
+import c632800 from './632800.json'
+import c640100 from './640100.json'
+import c640200 from './640200.json'
+import c640300 from './640300.json'
+import c640400 from './640400.json'
+import c640500 from './640500.json'
+import c650100 from './650100.json'
+import c650200 from './650200.json'
+import c652100 from './652100.json'
+import c652200 from './652200.json'
+import c652300 from './652300.json'
+import c652700 from './652700.json'
+import c652800 from './652800.json'
+import c652900 from './652900.json'
+import c653000 from './653000.json'
+import c653100 from './653100.json'
+import c653200 from './653200.json'
+import c654000 from './654000.json'
+import c654200 from './654200.json'
+import c654300 from './654300.json'
+import c659000 from './659000.json'
+import c710000 from './710000.json'
+import c810100 from './810100.json'
+import c820000 from './820000.json'
+
+export {
+    c110100.json,
+    c120100.json,
+    c130100.json,
+    c130200.json,
+    c130300.json,
+    c130400.json,
+    c130500.json,
+    c130600.json,
+    c130700.json,
+    c130800.json,
+    c130900.json,
+    c131000.json,
+    c131100.json,
+    c140100.json,
+    c140200.json,
+    c140300.json,
+    c140400.json,
+    c140500.json,
+    c140600.json,
+    c140700.json,
+    c140800.json,
+    c140900.json,
+    c141000.json,
+    c141100.json,
+    c150100.json,
+    c150200.json,
+    c150300.json,
+    c150400.json,
+    c150500.json,
+    c150600.json,
+    c150700.json,
+    c150800.json,
+    c150900.json,
+    c152200.json,
+    c152500.json,
+    c152900.json,
+    c210100.json,
+    c210200.json,
+    c210300.json,
+    c210400.json,
+    c210500.json,
+    c210600.json,
+    c210700.json,
+    c210800.json,
+    c210900.json,
+    c211000.json,
+    c211100.json,
+    c211200.json,
+    c211300.json,
+    c211400.json,
+    c220100.json,
+    c220200.json,
+    c220300.json,
+    c220400.json,
+    c220500.json,
+    c220600.json,
+    c220700.json,
+    c220800.json,
+    c222400.json,
+    c230100.json,
+    c230200.json,
+    c230300.json,
+    c230400.json,
+    c230500.json,
+    c230600.json,
+    c230700.json,
+    c230800.json,
+    c230900.json,
+    c231000.json,
+    c231100.json,
+    c231200.json,
+    c232700.json,
+    c310100.json,
+    c310200.json,
+    c320100.json,
+    c320200.json,
+    c320300.json,
+    c320400.json,
+    c320500.json,
+    c320600.json,
+    c320700.json,
+    c320800.json,
+    c320900.json,
+    c321000.json,
+    c321100.json,
+    c321200.json,
+    c321300.json,
+    c330100.json,
+    c330200.json,
+    c330300.json,
+    c330400.json,
+    c330500.json,
+    c330600.json,
+    c330700.json,
+    c330800.json,
+    c330900.json,
+    c331000.json,
+    c331100.json,
+    c340100.json,
+    c340200.json,
+    c340300.json,
+    c340400.json,
+    c340500.json,
+    c340600.json,
+    c340700.json,
+    c340800.json,
+    c341000.json,
+    c341100.json,
+    c341200.json,
+    c341300.json,
+    c341500.json,
+    c341600.json,
+    c341700.json,
+    c341800.json,
+    c350100.json,
+    c350200.json,
+    c350300.json,
+    c350400.json,
+    c350500.json,
+    c350600.json,
+    c350700.json,
+    c350800.json,
+    c350900.json,
+    c360100.json,
+    c360200.json,
+    c360300.json,
+    c360400.json,
+    c360500.json,
+    c360600.json,
+    c360700.json,
+    c360800.json,
+    c360900.json,
+    c361000.json,
+    c361100.json,
+    c370100.json,
+    c370200.json,
+    c370300.json,
+    c370400.json,
+    c370500.json,
+    c370600.json,
+    c370700.json,
+    c370800.json,
+    c370900.json,
+    c371000.json,
+    c371100.json,
+    c371200.json,
+    c371300.json,
+    c371400.json,
+    c371500.json,
+    c371600.json,
+    c371700.json,
+    c410100.json,
+    c410200.json,
+    c410300.json,
+    c410400.json,
+    c410500.json,
+    c410600.json,
+    c410700.json,
+    c410800.json,
+    c410900.json,
+    c411000.json,
+    c411100.json,
+    c411200.json,
+    c411300.json,
+    c411400.json,
+    c411500.json,
+    c411600.json,
+    c411700.json,
+    c420100.json,
+    c420200.json,
+    c420300.json,
+    c420500.json,
+    c420600.json,
+    c420700.json,
+    c420800.json,
+    c420900.json,
+    c421000.json,
+    c421100.json,
+    c421200.json,
+    c421300.json,
+    c422800.json,
+    c429000.json,
+    c430100.json,
+    c430200.json,
+    c430300.json,
+    c430400.json,
+    c430500.json,
+    c430600.json,
+    c430700.json,
+    c430800.json,
+    c430900.json,
+    c431000.json,
+    c431100.json,
+    c431200.json,
+    c431300.json,
+    c433100.json,
+    c440100.json,
+    c440200.json,
+    c440300.json,
+    c440400.json,
+    c440500.json,
+    c440600.json,
+    c440700.json,
+    c440800.json,
+    c440900.json,
+    c441200.json,
+    c441300.json,
+    c441400.json,
+    c441500.json,
+    c441600.json,
+    c441700.json,
+    c441800.json,
+    c441900.json,
+    c442000.json,
+    c445100.json,
+    c445200.json,
+    c445300.json,
+    c450100.json,
+    c450200.json,
+    c450300.json,
+    c450400.json,
+    c450500.json,
+    c450600.json,
+    c450700.json,
+    c450800.json,
+    c450900.json,
+    c451000.json,
+    c451100.json,
+    c451200.json,
+    c451300.json,
+    c451400.json,
+    c460100.json,
+    c460200.json,
+    c460300.json,
+    c469000.json,
+    c500100.json,
+    c510100.json,
+    c510300.json,
+    c510400.json,
+    c510500.json,
+    c510600.json,
+    c510700.json,
+    c510800.json,
+    c510900.json,
+    c511000.json,
+    c511100.json,
+    c511300.json,
+    c511400.json,
+    c511500.json,
+    c511600.json,
+    c511700.json,
+    c511800.json,
+    c511900.json,
+    c512000.json,
+    c513200.json,
+    c513300.json,
+    c513400.json,
+    c520100.json,
+    c520200.json,
+    c520300.json,
+    c520400.json,
+    c522200.json,
+    c522300.json,
+    c522400.json,
+    c522600.json,
+    c522700.json,
+    c530100.json,
+    c530300.json,
+    c530400.json,
+    c530500.json,
+    c530600.json,
+    c530700.json,
+    c530800.json,
+    c530900.json,
+    c532300.json,
+    c532500.json,
+    c532600.json,
+    c532800.json,
+    c532900.json,
+    c533100.json,
+    c533300.json,
+    c533400.json,
+    c540100.json,
+    c542100.json,
+    c542200.json,
+    c542300.json,
+    c542400.json,
+    c542500.json,
+    c542600.json,
+    c610100.json,
+    c610200.json,
+    c610300.json,
+    c610400.json,
+    c610500.json,
+    c610600.json,
+    c610700.json,
+    c610800.json,
+    c610900.json,
+    c611000.json,
+    c620100.json,
+    c620200.json,
+    c620300.json,
+    c620400.json,
+    c620500.json,
+    c620600.json,
+    c620700.json,
+    c620800.json,
+    c620900.json,
+    c621000.json,
+    c621100.json,
+    c621200.json,
+    c622900.json,
+    c623000.json,
+    c630100.json,
+    c632100.json,
+    c632200.json,
+    c632300.json,
+    c632500.json,
+    c632600.json,
+    c632700.json,
+    c632800.json,
+    c640100.json,
+    c640200.json,
+    c640300.json,
+    c640400.json,
+    c640500.json,
+    c650100.json,
+    c650200.json,
+    c652100.json,
+    c652200.json,
+    c652300.json,
+    c652700.json,
+    c652800.json,
+    c652900.json,
+    c653000.json,
+    c653100.json,
+    c653200.json,
+    c654000.json,
+    c654200.json,
+    c654300.json,
+    c659000.json,
+    c710000.json,
+    c810100.json,
+    c820000.json,
+}
