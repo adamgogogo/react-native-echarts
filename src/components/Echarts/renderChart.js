136225bbd3721c56c86368282a416ee569ecbe87
internet5 <172039711@qq.com>
2018-12-29 12:43:06 +0800

    Update renderChart.js

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -1,11 +1,18 @@
 import echarts from './echarts.min';
 import toString from '../../util/toString';
-import province from './map_data/province';
-import city from './map_data/city';
+import * as province from './map_data/province';
+import * as city from './map_data/city';
 
 export default function renderChart(props) {
   const height = `${props.height || 400}px`;
   const width = props.width ? `${props.width}px` : 'auto';
+  var map_data;
+  if (props.option.areaValue != null) {
+    if (props.option.codeType == 'province')
+      map_data = province[props.option.areaValue];
+    else
+      map_data = city['c' + props.option.areaValue]
+  }
   return `
     document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
